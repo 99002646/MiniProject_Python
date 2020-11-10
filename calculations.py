5757b3907bd567e6e810f779fec6f8974f0e4be2
99002622 <71603284+99002622@users.noreply.github.com>
2020-11-10 17:10:49 +0530

    sorts titles

--- calculations.py
+++ calculations.py
@@ -19,10 +19,23 @@ class Calculations(Players):
                 new_list.append(row[1])
         return sorted(new_list)
     
+    def sort_titles_in_desc(self):
+        """
+        Function to sort players titles in descending order.
+        """
+        data = self.import_data()
+        if data:
+            new_list = []
+            for row in data:
+                new_list.append(row[4])
+        return sorted(new_list, reverse=True)
+
+    
     
 
 DATA = Calculations('top_women_chess_players_aug_2020.csv')
 print(DATA.import_data())
 print(DATA.display())
 print(DATA.sort_names_in_asc())
+print(DATA.sort_titles_in_desc())
 
