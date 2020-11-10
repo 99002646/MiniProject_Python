45d93f5c19018e597ab45cbe2c1aa6b0d2ae37e1
99002622 <71603284+99002622@users.noreply.github.com>
2020-11-10 17:01:14 +0530

    displays the data

--- dataset.py
+++ dataset.py
@@ -27,3 +27,20 @@ class Players:
             print("file doesn't exist")
         else:
             return self.data_list
+     def display(self):
+        """
+        This method is responsible for displaying all the data row-wise
+        """
+        for row in self.data_list:
+            print("Fide ID:", row[0])
+            print("Name:", row[1])
+            print("Federation:", row[2])
+            print("Gender:", row[3])
+            print("Title:", row[4])
+            print("DOB:", row[5])
+            print("Standard rating:", row[6])
+            print("Rapid rating:", row[7])
+            print("Blitz rating:", row[8])
+            print("\n")
+        
+      
