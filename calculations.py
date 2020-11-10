ef635878f309b39862359c547c0e107cb7366a75
99002567 <71582894+99002567@users.noreply.github.com>
2020-11-10 17:08:46 +0530

    sorts the names in ascending order

--- calculations.py
+++ calculations.py
@@ -8,3 +8,21 @@ class Calculations(Players):
     """
     def __init__(self, my_list):
         Players.__init__(self, my_list)
+    def sort_names_in_asc(self):
+        """
+        Function to sort names in ascending order.
+        """
+        data = self.import_data()
+        if data:
+            new_list = []
+            for row in data:
+                new_list.append(row[1])
+        return sorted(new_list)
+    
+    
+
+DATA = Calculations('top_women_chess_players_aug_2020.csv')
+print(DATA.import_data())
+print(DATA.display())
+print(DATA.sort_names_in_asc())
+
